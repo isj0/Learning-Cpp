afeae279d362188bb009b22c6c3b85a736e3fd5a
Isj <iqbal.singh23@gmail.com>
2025-03-03 23:42:09 -0500

    upadted vscode config files

---
+++ 01_Basics/Proj2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num{100};
+    cout << "Hello from Project 2" << endl;
+    cout << "Enter your favorite number: ";
+    cin >> num;
+    cout << num << " * 2 = " << num * 2 << endl;
+
+    return 0;
+}
